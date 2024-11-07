2edec394c7e5b639f5f10825391c7436099ce6d8
khatz <kchatzis18@gmail.com>
2024-11-07 15:01:12 +0200

    build an api for updating issues

--- app/issues/IssueForm.tsx
+++ app/issues/IssueForm.tsx
@@ -8,12 +8,12 @@ import { useState } from "react";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { Issue } from "@prisma/client";
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 interface Props {
   issue?: Issue;
@@ -27,7 +27,7 @@ export default function IssueForm({ issue }: Props) {
     control,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
 
   const [error, setError] = useState("");
