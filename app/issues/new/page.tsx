48582cc50480a49ca93b4c2f5a7f87a63007e8e4
khatz <kchatzis18@gmail.com>
2024-11-10 12:23:00 +0200

    dynamic import IssueForm

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,11 @@
-import IssueForm from "../IssueForm";
+import dynamic from "next/dynamic";
+
+const IssueForm = dynamic(() => import("../IssueForm"), { ssr: false });
 
 export default function NewIssuePage() {
-  return <IssueForm />;
+  return (
+    <>
+      <IssueForm />
+    </>
+  );
 }
