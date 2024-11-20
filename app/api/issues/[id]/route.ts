37c1894d5f80a93afdff7c5a43c7d2ad4f7de2a0
khatz <kchatzis18@gmail.com>
2024-11-20 14:39:56 +0200

    add the option to assign issues on the API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -13,9 +13,17 @@ export async function PATCH(request: NextRequest, { params }: Props) {
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid User" }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -30,6 +38,7 @@ export async function PATCH(request: NextRequest, { params }: Props) {
       title: body.title,
       description: body.description,
       status: body.status,
+      assignedToUserId: body.assignedToUserId,
     },
   });
 
