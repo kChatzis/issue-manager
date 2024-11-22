4d4f134f48745c9ad88d13952bec030776d22ed5
khatz <kchatzis18@gmail.com>
2024-11-22 21:35:17 +0200

    re-enable some commented code

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -5,8 +5,8 @@ import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: NextRequest) {
-  // const session = await getServerSession(authOptions);
-  // if (!session) return NextResponse.json({}, { status: 401 });
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success)
